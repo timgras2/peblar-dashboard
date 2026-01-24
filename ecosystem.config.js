5d646ab64cd35fc899b0085291135fdb4fca76d1
Tim Grasmaijer <tgra@superp.local>
2026-01-24 23:14:59 +0100

    Update ecosystem config to direct next biary for Windows PM2 stability

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,8 @@
 module.exports = {
     apps: [{
         name: "peblar",
-        script: "npm",
-        args: "run start -- -H 0.0.0.0",
-        shell: true,
+        script: "./node_modules/next/dist/bin/next",
+        args: "start -H 0.0.0.0",
         env: {
             NODE_ENV: "production",
         }
